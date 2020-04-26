34db9c45b168b0b383a6e46dd6911ca391bba3ab
Krishna-adk <krishna.forkhaki@gmail.com>
2020-04-26 14:33:09 +0545

    Widgets included

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:forkhakifoods/widgets/catagories_widget.dart';
 import 'package:forkhakifoods/widgets/crousel_widget.dart';
--- lib/widgets/flash_sell_widget.dart
+++ lib/widgets/flash_sell_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:forkhakifoods/pages/HomePage.dart';
 
 class FlashSell extends StatefulWidget {
   @override
